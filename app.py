8c00aeb2abecb98b6b6daf8e748840e462d7ef51
Codel1417 <awoo@codel1417.xyz>
2022-11-26 23:38:33 -0700

    Auto open fullscreen browser window

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ import time
 from quart import Quart, render_template, websocket
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
+from selenium.webdriver.firefox.options import Options
+
 from EyeData import EyeData
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+options = Options()
+options.page_load_strategy = 'none'
+
+driver = webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()))
+driver.fullscreen_window()
 
 app: Quart = Quart(__name__)
 dispatcher: Dispatcher = Dispatcher()
@@ -60,7 +71,6 @@ async def ws():
         await websocket.send(data)
         await asyncio.sleep(0.01)
 
-
 @app.before_serving
 async def startup():
     global server
@@ -68,6 +78,7 @@ async def startup():
     loop = asyncio.get_event_loop()
     server = AsyncIOOSCUDPServer(("0.0.0.0", 9000), dispatcher, loop)
     transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
+    driver.get("http://127.0.0.1:5000")
 
 
 @app.after_serving
@@ -75,7 +86,7 @@ async def shutdown():
     global transport
     if transport is not None:
         transport.close()
-
+    driver.quit()
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=False)
+    app.run(debug=True, threaded=False, use_reloader=False)
