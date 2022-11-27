ac72473556c5d0e36733520c909a0a5eec6dc84e
Codel1417 <awoo@codel1417.xyz>
2022-11-26 23:05:52 -0700

    add fallback/idle mode if no OSC data is sent for 15 seconds

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 import asyncio
-from quart import Quart, render_template, websocket, g
+import random
+import time
+
+from quart import Quart, render_template, websocket
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 from EyeData import EyeData
 
-app = Quart(__name__)
-dispatcher = Dispatcher()
-eyeData = EyeData()
+app: Quart = Quart(__name__)
+dispatcher: Dispatcher = Dispatcher()
+eyeData: EyeData = EyeData()
+lastOSCMessage: float = time.time() - 20
+lastEyeTime: float = time.time() - 20
+eyeAutoTime: float = 5
+previousJson: str = ""
+server = None
+transport = None
+
 
 async def default_handler(address, *args):
     print(f"DEFAULT {address}: {args}")
 
 
 def print_handler(address, *args):
-    print(f"{address}: {args}")
+    global lastOSCMessage
+    global eyeData
     eyeData.eyeX = args[0]
     eyeData.eyeY = args[1]
+    lastOSCMessage = time.time()
 
 
 dispatcher.map("/xy1", print_handler)
@@ -29,30 +41,41 @@ async def eyes():  # put application's code here
 
 @app.websocket("/ws")
 async def ws():
-    previousJson = g.get("previousJson")
+    global lastOSCMessage
+    global lastEyeTime
+    global eyeAutoTime
+    global previousJson
+    global eyeData
     while True:
+        currentTime = time.time()
+        timeSince = currentTime - lastOSCMessage
+        if timeSince > 15:
+            timeSince = currentTime - lastEyeTime
+            if timeSince > eyeAutoTime:
+                eyeData.eyeX = random.uniform(0.3, 0.7)
+                eyeData.eyeY = random.uniform(0.3, 0.7)
+                eyeAutoTime = random.uniform(0.3, 4)
+                lastEyeTime = time.time()
         data = eyeData.toJson()
-        if data != previousJson:
-            await websocket.send(data)
-            g.setdefault("previousJson",previousJson)
+        await websocket.send(data)
+        await asyncio.sleep(0.01)
 
 
 @app.before_serving
 async def startup():
-    print("Starting OSC")
+    global server
+    global transport
     loop = asyncio.get_event_loop()
     server = AsyncIOOSCUDPServer(("0.0.0.0", 9000), dispatcher, loop)
     transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
-    g.setdefault("osc", transport)
 
 
 @app.after_serving
 async def shutdown():
-    print("Stopping OSC")
-    transport = g.get("osc")
-    if transport != None:
+    global transport
+    if transport is not None:
         transport.close()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=False)
